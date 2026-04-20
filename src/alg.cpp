f7974720b8716deae20e188f92f1d2dcedc10bd7
RaweNist <116841332+RaweNist@users.noreply.github.com>
2026-04-20 11:13:39 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -11,6 +11,7 @@ std::string infx2pstfx(const std::string& inf) {
   for (int i = 0; i < inf.size(); ++i) {
     if (numbers.find(inf[i]) <= 10) {
       res += inf[i];
+      res += ' ';
     } else if (operands.find(inf[i]) < operands.size()) {
       int op, op_temp;
       if (inf[i] == '(') {
@@ -26,13 +27,15 @@ std::string infx2pstfx(const std::string& inf) {
           temp.push(inf[i]);
         } else if (inf[i] == ')') {
           while (temp.get() != '(') {
-              res += temp.get();
-              temp.pop();
+            res += temp.get();
+            res += ' ';
+            temp.pop();
           }
           temp.pop();
         } else {
           while (op_temp >= op) {
             res += temp.get();
+            res += ' ';
             temp.pop();
             if (!temp.isEmpty()) {
               op_temp = operands.find(temp.get());
@@ -51,6 +54,7 @@ std::string infx2pstfx(const std::string& inf) {
   }
   while (!temp.isEmpty()) {
     res += temp.get();
+    res += ' ';
     temp.pop();
   }
   return res;
@@ -62,25 +66,27 @@ int eval(const std::string& pref) {
   tstack<int, 100> temp1;
   std::string str = infx2pstfx(pref);
   for (int i = 0; i < str.size(); ++i) {
-    if (numbers.find(str[i]) <= 10) {
+    if (str[i] != ' ') {
+      if (numbers.find(str[i]) <= 10) {
       temp1.push(str[i] - '0');
-    } else if (operands.find(str[i]) < operands.size()) {
-      int b = temp1.get();
-      temp1.pop();
-      int a = temp1.get();
-      temp1.pop();
-      int ab = 0;
-      switch (str[i]) {
-      case '+': ab = a + b; break;
-      case '-': ab = a - b; break;
-      case '*': ab = a * b; break;
-      case '/': ab = a / b; break;
-      default:
-        break;
+      } else if (operands.find(str[i]) < operands.size()) {
+        int b = temp1.get();
+        temp1.pop();
+        int a = temp1.get();
+        temp1.pop();
+        int ab = 0;
+        switch (str[i]) {
+        case '+': ab = a + b; break;
+        case '-': ab = a - b; break;
+        case '*': ab = a * b; break;
+        case '/': ab = a / b; break;
+        default:
+          break;
+        }
+        temp1.push(ab);
+      } else {
+        throw std::string("Wrong element format");
       }
-      temp1.push(ab);
-    } else {
-      throw std::string("Wrong element format");
     }
   }
   return temp1.get();
